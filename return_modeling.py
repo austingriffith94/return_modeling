21192cac28b4351d5d8b2b43fa3c1b86d1030912
austingriffith94 <austgriffia@gmail.com>
2017-12-01 11:22:02 -0500

    updated csv headers

--- return_modeling.py
+++ return_modeling.py
@@ -15,7 +15,7 @@ class reader:
         self.name = name
         self.min = min
         self.data = pd.DataFrame.empty
-        
+
     def csvr(self):
         data = pd.read_csv(self.name)
         data = data.drop_duplicates(['Symbol'])
@@ -27,20 +27,20 @@ class reader:
         data['Cap'] = (data['Val']*(10**9)).where(data['Dol'] == 'B')
         data['mil'] = (data['Val']*(10**6)).where(data['Dol'] == 'M')
         data['Cap'] = data['Cap'].fillna(data['mil'])
-        data = data[data['Cap'] >= self.min]  
+        data = data[data['Cap'] >= self.min]
         self.data = data
         return(data)
-        
+
     def tickers(self):
         t = self.data['Symbol']
         return(t)
-        
-# combines ticker data frames     
+
+# combines ticker data frames
 def combine_tickers(t1,t2):
     all_t = [t1,t2]
     ticker = pd.concat(all_t).reset_index(drop=True)
     return(ticker)
-        
+
 # pulls api data and saves it as hdf5
 # reads hdf5 api data file
 class api:
@@ -49,7 +49,7 @@ class api:
         self.source = source
         self.start = start
         self.end = end
-    
+
     def api_save(self):
         api_data = pdr.DataReader(self.tickers,self.source,self.start,self.end)
         api_data.to_hdf('api_data.h5','api_data',format = 't')
@@ -57,13 +57,13 @@ class api:
     def api_read(self):
         file = pd.read_hdf('api_data.h5')
         return(file)
-        
-# calculates x and y variables used in modeling of adjusted close returns    
-class xy:    
+
+# calculates x and y variables used in modeling of adjusted close returns
+class xy:
     def __init__(self,date,adj):
         self.date = date
         self.adj = adj
-    
+
     # log returns of adj close price
     def log_ret(self,x):
         adj = self.adj
@@ -71,25 +71,25 @@ class xy:
         for i in x:
             loc = adj.index.get_loc(self.date)
             adj_date = adj.iloc[loc]
-            
+
             if i > adj.iloc[0:loc].shape[0]:
                 window = adj[adj.index < self.date].iloc[0]
             else:
                 window = adj[adj.index < self.date].iloc[-i]
-                
+
             loc1 = adj.index.get_loc(window.name)
             adj_1 = adj.iloc[loc1]
             adj_log = np.log(adj_date/adj_1)
             log = pd.concat([log,adj_log], axis=1)
-        
+
         header = list(map(str,x))
         y_head = []
         for i in header:
             y_head = y_head + ['y' + i]
-            
+
         log.columns = y_head
         return(log)
-    
+
     # moving average over specified period of days
     def move_avg(self,x):
         adj = self.adj
@@ -102,20 +102,20 @@ class xy:
                 window2 = adj[adj.index < date].iloc[0]
             else:
                 window2 = adj[adj.index < date].iloc[-i]
-            
+
             loc2 = adj.index.get_loc(window2.name)
             adj_range = adj.iloc[loc2:loc1+1]
             adj_mean = adj_range.mean()
             ma = pd.concat([ma,adj_mean], axis=1)
-            
+
         header = list(map(str,x))
         ma_head = []
         for i in header:
             ma_head = ma_head + ['ma' + i]
-            
+
         ma.columns = ma_head
         return(ma)
-    
+
     # log returns lagged by one day from specified date
     def lag_log_ret(self,x):
         adj = self.adj
@@ -125,25 +125,25 @@ class xy:
             window = adj[adj.index < date].iloc[-1]
             loc = adj.index.get_loc(window.name)
             adj_date = adj.iloc[loc]
-            
+
             if i > adj.iloc[0:loc].shape[0]:
                 window2 = adj[adj.index < self.date].iloc[0]
             else:
                 window2 = adj[adj.index < self.date].iloc[-i]
-                
+
             loc1 = adj.index.get_loc(window2.name)
             adj_1 = adj.iloc[loc1]
             adj_log = np.log(adj_date/adj_1)
             log = pd.concat([log,adj_log], axis=1)
-        
+
         header = list(map(str,x))
         pa = []
         for i in header:
             pa = pa + ['pa' + i]
-            
+
         log.columns = pa
         return(log)
-        
+
 
 
 
@@ -172,6 +172,7 @@ file = yahoo.api_read()
 # date list
 # pulls adjusted close price from api data panel
 d = ['2016-06-01','2017-07-03']
+length = 60
 adj = file.loc['Adj Close']
 
 # distances in log, average and lagged log
@@ -188,13 +189,13 @@ for date in d:
     dfp5 = pd.DataFrame([])
     dfp1 = pd.DataFrame([])
     dfp22 = pd.DataFrame([])
-    
+
     # getting dates
     loc = adj.index.get_loc(date)
-    adj1 = adj[loc:loc+60]
+    adj1 = adj[loc:loc+length]
     adj1 = pd.DataFrame.transpose(adj1)
     dates = list(adj1.columns.values)
-    
+
     # for loop through all dates
     # executes x and y variables of classes
     for time in dates:
@@ -225,7 +226,7 @@ for date in d:
             elif yh == list(log_n)[2]:
                 dfcoef22 = pd.concat([dfcoef22,coef],axis=1)
                 dfp22 = pd.concat([dfp22,p_val],axis=1)
-    
+
     # transposes bins, takes average of values
     df1 = dfcoef1.transpose().mean()
     df5 = dfcoef5.transpose().mean()
@@ -236,9 +237,16 @@ for date in d:
     dfp1 = dfp1.transpose().mean()
     dfp5 = dfp5.transpose().mean()
     dfp22 = dfp22.transpose().mean()
-    
+
+    # labels for variables in csv
+    l0 = list(log_n)[0]
+    l1 = list(log_n)[1]
+    l2 = list(log_n)[2]
+    s = 'std dev'
+    p = 'p_val'
+
     # concatinates values into main variable dataframe
     # renames columns and outputs data frame to csv with the date as the name
     main = pd.concat([df1,df5,df22,dfstd1,dfstd5,dfstd22,dfp1,dfp5,dfp22],axis=1)
-    main.columns = [list(log_n)[0],list(log_n)[1],list(log_n)[2],'std1','std5','std22','p1','p5','p22']
+    main.columns = [l0,l1,l2,l0+s,l1+s,l2+s,l0+p,l1+p,l2+p]
     main.to_csv(date+'.csv')
